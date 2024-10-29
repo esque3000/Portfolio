7aea6a718c5b565e64180778c8b14bbb80246640
Jiten Ukani <jukani9000@gmail.com>
2024-10-29 15:35:08 +0000

    Added Basic Character Locomotion with movement traversal logic that allows the player to mantle/vault actors with the Movement Traversal Component

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Portfolio
+# Portfolio Project
+
+## Background
+This is an Unreal Engine project that I have been working on in my spare time for the purpose of demonstating my skills as a Senior Software Engineer. I have also used this project to experiment with various features for my own understanding of how they can be used and applied to games.
+
+## How to use
+- Clone the repo onto your machine and then right-click the uproject file and generate project files for your specific IDE. [^1]
+- Load the project in the editor and run PIE with the default level.
+
+## Demonstated Features
+`TODO`
+
+***
+[^1]: For the purposes of this project I have used VSCode but due to the way Ureal creates the workspace all the directories in the settings are hard coded to the location of where I have the engine installed.
---
+++ Source/Portfolio/Character/PortfilioCharacter.h
@@ -0,0 +1,52 @@
+// Copyright Jiten Ukani 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PortfilioCharacter.generated.h"
+
+UCLASS()
+class PORTFOLIO_API APortfilioCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext *DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction *JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction *MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction *LookAction;
+
+public:
+	// Sets default values for this character's properties
+	APortfilioCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** Called for movement input */
+	void Move(const struct FInputActionValue &Value);
+
+	/** Called for looking input */
+	void Look(const struct FInputActionValue &Value);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+	virtual void PossessedBy(AController *NewController) override;
+};
